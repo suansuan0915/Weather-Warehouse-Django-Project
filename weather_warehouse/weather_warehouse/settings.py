c70db2c878a4e3e5f9c3c50de93332bc9806e9a6
wsa50 <wsa50@sfu.ca>
2023-03-17 18:22:18 -0400

    Update settings.py
    export SECRET_KEY for django

--- weather_warehouse/weather_warehouse/settings.py
+++ weather_warehouse/weather_warehouse/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*^%ic6e3hw*+mhs7es_a2t4%3*9egqq$q19o!-t@t&*(g6$^p0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
